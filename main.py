6d4f2b8dadeaa944fb194112fd1b7f8d7ada11e9
Luke <lukefernandes.lsf@gmail.com>
2022-06-06 13:52:19 -0700

    typer test

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import board
 import neopixel
 import time
+import typer
+
+app = typer.Typer()
 
 pixels = neopixel.NeoPixel(board.D18, 30)
 
-pixels[0] = (255,0,0)
-time.sleep(3)
-pixels[0] = (0,0,0)
+
+@app.command
+def turnOnTest():
+    pixels.fill((0, 0, 0))
+    pixels.fill((255, 0, 0))
+
+@app.command
+def turnOffTest():
+    pixels.fill((0, 0, 0))
+
+
+if __name__ == "__main__":
+    app()
